9ca46720433d64dfa4446f795bf90b5225d4cb91
Yu-Jhe Li <yujhe.jack@gmail.com>
2022-06-05 01:00:23 -0400

    add missing copy right

--- prod_lib/engine/probe.py
+++ prod_lib/engine/probe.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 from detectron2.structures import pairwise_iou
 
 class OpenMatchTrainerProbe:
--- prod_lib/runner/__init__.py
+++ prod_lib/runner/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+
 # from .runner import SemiSupSegRunner, SemiSupHandTrackingRunner  # noqa
 from .runner import BaseUnbiasedTeacherRunner  # noqa
 from .runner import DAobjUnbiasedTeacherRunner # noqa
